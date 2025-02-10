01e490d03f9574a124bf0ee82a68d861afb30c6e
bonmin9 <bonmin411@gmail.com>
2025-02-10 20:53:59 +0900

    refactor: 코드 로직 수정

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,7 @@ import os
 
 def create_app():
     # .env 파일 로드
-    load_dotenv()
+    load_dotenv('config.env')
 
     # Flask 애플리케이션 생성
     app = Flask(__name__)
@@ -13,16 +13,14 @@ def create_app():
     app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'default_fallback_key')
 
     # Blueprint 등록
-    from .routes import main, data_collector_bp, recommendation_bp  # recommendation_bp 추가
+    from .routes import api_bp  # recommendation_bp 추가
     '''
     google_custom_search.py, openai_processor.py의 blueprint를 지정하지 않는 이유
     - google_custom_search.py, openai_processor.py는 Flask 엔드포인트(API 엔드포인트)를 직접 제공하지
     않고, 비즈니스 로직(데이터 처리, API 요청 등)만 수행
     - routes.py에서 직접 해당 모듈의 함수를 호출하기 때문에 Flask 앱과 직접적인 연관이 없음
     '''
-    app.register_blueprint(main)
-    app.register_blueprint(data_collector_bp)
-    app.register_blueprint(recommendation_bp)
+    app.register_blueprint(api_bp)
 
     return app
 
--- app/services/google_custom_search.py
+++ app/services/google_custom_search.py
@@ -1,21 +1,20 @@
 import os
 import requests
-from json import dumps
-from app.services.openai_processor import generate_description  # OpenAI API 함수 추가
 
+import dotenv
+dotenv.load_dotenv('config.env')
 Google_SEARCH_ENGINE_ID = os.getenv('Google_SEARCH_ENGINE_ID')
 Google_API_KEY = os.getenv('Google_API_KEY')
 
-def get_snippet(category: str, name: str, address: str, link: str, page: int=1) -> dict:
+def get_snippet(category: str, title: str, place: str) -> dict:
     """
     Google Custom Search API를 사용하여 검색 스니펫을 가져오고, OpenAI API를 호출하여 설명을 생성하는 함수.
 
     Args:
         category(str): 여가 활동의 카테고리
-        name(str): 여가 활동의 이름
+        title(str): 여가 활동의 이름
         address(str): 장소명/건물명(없을 경우 공백문자로 입력)
         link(str): 네이버 API에서 가져온 원본 링크
-        page(int): 로드할 페이지의 수(default=1)
 
     Returns:
         dict: OpenAI API에서 생성된 설명을 포함한 JSON 데이터
@@ -27,24 +26,13 @@ def get_snippet(category: str, name: str, address: str, link: str, page: int=1) 
         }
     
     # 검색어 설정
-    query = name if category == "팝업스토어" else f"{address} {name}"
-    url = f"https://www.googleapis.com/customsearch/v1?key={Google_API_KEY}&cx={Google_SEARCH_ENGINE_ID}&q={query}&start={page}"
+    query = title if category == "팝업스토어" else f"{place} {title}"
+    url = f"https://www.googleapis.com/customsearch/v1?key={Google_API_KEY}&cx={Google_SEARCH_ENGINE_ID}&q={query}&start=1"
 
     response = requests.get(url)
 
     if response.status_code == 200:
-        snippets = [item['snippet'] for item in response.json().get('items', [])]
-
-        # OpenAI API를 사용하여 설명 생성
-        description_data = generate_description(
-            category=category,
-            title=name,
-            address=address,
-            link=link,  # 네이버 API에서 받은 원본 링크 사용
-            snippets=snippets
-        )
-
-        return description_data
+        return [item['snippet'] for item in response.json().get('items', []) if 'snippet' in item]
     
     return {
         "code": "API_ERROR",
