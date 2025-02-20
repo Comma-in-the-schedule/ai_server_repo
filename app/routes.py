2ef3d6246fb08604786e1838fc459d0cf4d04eeb
bonmin9 <bonmin411@gmail.com>
2025-02-20 18:09:46 +0900

    refactor: 카테고리 별로 다른 로직을 사용하도록 수정

--- app/routes.py
+++ app/routes.py
@@ -1,51 +1,21 @@
 from flask import Blueprint, jsonify, request
-from app.services.data_collector import collect_data
-from app.services.google_custom_search import get_snippet
-from app.services.openai_processor import generate_description
+from app.services.data_collectors.naver_collector import collect_data
+from app.services.snippets_collector import get_snippet
+from app.services.description_generator import generate_description
+from app.services.period_processor import is_free_time_in_period
 from app.services.recommender import recommend
 
-api_bp = Blueprint('api', __name__)
+def process_popupstore(location, free_time):
+    result = collect_data(location, "팝업스토어")
 
-@api_bp.route('/ai/recommendation', methods=['POST'])
-def run_main():
-    data = request.get_json()
-
-    location = data.get("location")
-    free_time = data.get("free_time")
-    category_nums = data.get("category")  # 리스트 형태로 받을 수 있도록 수정
-
-    if not location or not free_time or not category_nums:
-        return jsonify({"code": "VF", "message": "Validation failed. Location, free_time and category are required."}), 400
-
-    # category_nums가 단일 숫자(int)로 들어오면 리스트로 변환
-    if isinstance(category_nums, int):
-        category_nums = [category_nums]
-
-    category_list = {1: "팝업스토어", 2: "전시회", 3: "영화", 4: "공방"}
+    if result["code"] != "SU":
+        return {"code": "SE", "message": f"system_error: N-{result['code']}"}
     
-    collected_data = []
+    elif not result["message"]:
+        return {"code": "NF", "message": "No data found for the given criteria."}
     
-    for category_num in category_nums:
-        category = category_list.get(category_num)
-
-        if not category:
-            continue  # 잘못된 카테고리 번호 무시
-        
-        # 네이버 API에서 기본 데이터 수집
-        if category == "팝업스토어":
-            data_result = collect_data(location, category)
-        elif category == "전시회":
-            data_result = {"code": "SU", "message": []}  # 다중 카테고리 반환 테스트 용. 전시회 데이터 없음 처리
-        else:
-            data_result = {"code": "SU", "message": []}  # 다중 카테고리 반환 테스트 용.
-
-        if data_result["code"] != "SU":
-            return jsonify({"code": "SE1", "message": f"system_error: N-{data_result['code']}"}), 400
-        
-        collected_data.extend(data_result["message"])
-
-    if not collected_data:
-        return jsonify({"code": "NF", "message": "No data found for the given criteria."})
+    else:
+        collected_data = result["message"]
 
     event_list = []
     for item in collected_data:
@@ -61,7 +31,7 @@ def run_main():
         )
 
         if snippets["code"] != "SU":
-            return jsonify({"code": "SE2", "message": f"system_error: G-{snippets['code']}"}), 400
+            return {"code": "SE", "message": f"system_error: G-{snippets['code']}"}
         
         full_data = generate_description(
             category=category, 
@@ -72,12 +42,40 @@ def run_main():
             opening_time='', 
             url=url, 
             snippets=snippets["message"])
-        
-        event_list.append(full_data)
 
-    recommendation = recommend(free_time, event_list)
+        if is_free_time_in_period(free_time, full_data['period']) or not full_data['period']:  # 운영 기간이 여가 시간에 포함될 경우, 운영 기간 데이터가 없을 경우에만 추가
+            event_list.append(full_data)
 
-    if recommendation[0]['title'] == "None":
-        return jsonify({"code": "NF", "message": "No data found for the given criteria."})
+    return {"code": "SU", "message": "Success.", "result": event_list}
 
-    return jsonify({"code": "SU", "message": "Success.", "result": recommendation})
+def process_exhibition(location, free_time):
+    # 테스트를 위한 임시 더미데이터
+    pass
+
+api_bp = Blueprint('api', __name__)
+
+@api_bp.route('/ai/recommendation', methods=['POST'])
+def run_main():
+    data = request.get_json()
+
+    location = data.get("location")
+    free_time = data.get("free_time")
+    categories = data.get("category")  # 리스트 형태로 받을 수 있도록 수정
+
+    if not location or not free_time or not categories:
+        return jsonify({"code": "VF", "message": "Validation failed. Location, free_time and category are required."}), 400
+
+    # category_nums가 단일 숫자(int)로 들어오면 리스트로 변환
+    if isinstance(categories, int):
+        categories = [categories]
+
+    result_list = [["No Data"], ["No Data"]]
+
+    for category in categories:
+        # 네이버 API에서 기본 데이터 수집
+        if category == 1:
+            result_list[0] = process_popupstore(location, free_time)["result"]
+        # elif category == 2:
+        #     result_list[1] = process_exhibition(location, free_time)["result"]
+
+    return jsonify({"code": "SU", "message": "Success.", "result": result_list})
